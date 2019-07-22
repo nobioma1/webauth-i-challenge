64672c66820245b00936cd4e6af770e625775361
Noble Obioma <nobioma1@gmail.com>
2019-07-22 12:30:39 +0100

    Correct migrations, seeds file path in config

--- server/knexfile.js
+++ server/knexfile.js
@@ -11,10 +11,10 @@ module.exports = {
       },
     },
     migrations: {
-      directory: './database/migrations',
+      directory: './data/migrations',
     },
     seeds: {
-      directory: './database/seeds',
+      directory: './data/seeds',
     },
   },
 };
