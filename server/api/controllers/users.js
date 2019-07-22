04ad9ff8e01a2a2a0356a18159464911a7a6aea6
Noble Obioma <nobioma1@gmail.com>
2019-07-22 14:37:12 +0100

    Create controllers to create a new users and get all users

--- server/api/controllers/users.js
+++ server/api/controllers/users.js
@@ -1,7 +1,29 @@
-function userRegister() {}
+const generatePassword = require('../helper/generatePassword');
+const { addUser, getUsers } = require('../models/usersModel');
 
-function loginUser() {}
+const userRegister = async (req, res) => {
+  const { username, password } = req.body;
+  const newUser = {
+    username,
+    password: generatePassword(password),
+  };
+  try {
+    const user = await addUser(newUser);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Error Creating User' });
+  }
+};
 
-function getUsers() {}
+const loginUser = async (req, res) => {};
 
-module.exports = { userRegister, getUsers, loginUser };
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await getUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Error getting User' });
+  }
+};
+
+module.exports = { userRegister, getAllUsers, loginUser };
