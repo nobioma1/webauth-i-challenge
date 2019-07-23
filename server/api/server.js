94c4a61681fd81ffd769e9a11526a4153b8377da
Noble Obioma <nobioma1@gmail.com>
2019-07-23 12:43:04 +0100

    Create session config

--- server/api/server.js
+++ server/api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 
 const userRouter = require('../api/routes/userRoutes');
 
@@ -7,6 +8,19 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
+server.use(
+  session({
+    name: 'sessionId',
+    secret: `shhh, this is my secret! don't tell my mom`,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+      httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
 
 server.use('/api', userRouter);
 
